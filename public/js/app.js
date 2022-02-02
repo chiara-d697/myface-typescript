cb7c98633d9a0c9c01a38dd32683aefd7c1ed964
Chiara Dinan <Chiara.Dinan@softwire.com>
2022-02-02 15:40:08 +0000

    added JS functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript here.
+
 });
+
+function addPostColour() {
+    let posts = document.getElementsByClassName('post-block');
+
+    for(let i = 0; i < posts.length; i++) {
+        posts[i].style.background = 
+    `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    }
+}
+
+function toggleMobileMenu() {
+    let elem = document.getElementById('mobile-menu');
+    elem.classList.toggle('mobile-menu-shown');
+}
